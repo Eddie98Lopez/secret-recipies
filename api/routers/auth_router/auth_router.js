4a599362d3745d2cb1fcb126d65c7ec23eeedcdc
Eduardo Lopez <ed98lopez@gmail.com>
2021-04-27 13:43:00 -0700

    added router folder. added router/auth_router/ middleware and auth_router. working on post.register, post.login, and get.logout as well as validation middleware

---
+++ api/routers/auth_router/auth_router.js
@@ -0,0 +1,15 @@
+const router = require('express').Router()
+
+router.post("/register", async(req,res)=>{
+
+})
+
+router.post("/login", async(req,res)=>{
+
+})
+
+router.get("/logout", async(req,res)=>{
+
+})
+
+module.exports = router
