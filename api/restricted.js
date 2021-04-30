c5461b5479d367b835d81e4233f4e4a8de2084de
Eduardo Lopez <ed98lopez@gmail.com>
2021-04-30 12:10:19 -0700

    added restricting middleware that requires valid token to access recipe related endpoints

---
+++ api/restricted.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../secret')
+
+const restricted = (req,res,next) =>{
+
+    const token = req.headers.authorization
+
+    if(!token){
+        res.status(403).json('token is required')
+    }
+    else{
+        jwt.verify(token,JWT_SECRET,(err,decoded)=>{
+            if(err){
+                res.status(400).json('token invalid')
+            }
+            else{
+                req.decoded = decoded
+                next()
+            }
+        })
+    }
+
+}
+
+module.exports = {restricted}
